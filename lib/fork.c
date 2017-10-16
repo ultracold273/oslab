8405758b31d3a973bca2b324dea796608ba548c2
ultracold <ultracold@outlook.com>
2017-10-16 15:33:42 +0800

    error fix fork.c

--- lib/fork.c
+++ lib/fork.c
@@ -71,7 +71,7 @@ duppage(envid_t envid, unsigned pn)
 	// LAB 4: Your code here.
 	//panic("duppage not implemented");
 	pte_t pte = uvpt[pn];
-	void * va = pn * PGSIZE;
+	void * va = (void *) (pn * PGSIZE);
 	if ((pte & PTE_W) == PTE_W || (pte & PTE_COW) == PTE_COW) {
 		if ((r = sys_page_map(0, va, envid, va,(PTE_U | PTE_P | PTE_COW))) < 0) {
 			return r;
